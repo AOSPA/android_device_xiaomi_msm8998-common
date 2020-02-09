7a0a55c06bbd4544a622b8c6b8fcc87ee28a46d5
Arian <arian.kulmer@web.de>
2020-02-09 15:02:02 +0100

    msm8998-common: Ship MiuiCamera
    Change-Id: Icc98d8b6abfd26b89ed1ac882590c8013d09588d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,3 +175,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from proprietary files
 include vendor/xiaomi/msm8998-common/BoardConfigVendor.mk
+include vendor/miuicamera/common/BoardConfigVendor.mk
